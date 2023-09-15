5478456f96ae2c28822fa0c80712af0c53ab1ba2
Priyansh Srivastava <priyansh0206@gmail.com>
2023-09-16 01:00:32 +0530

    Add the CSS and JS assets for home EJS

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const port = 8000;
 
 const app = express();
 
+// function for setting up the routes and controller
 app.use('/', require('./routes/routesIndex'));
 
+// function for setting up the views
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Function for running the server
 app.listen(port, function(err){
     if(err){
         console.log("Error is :");
